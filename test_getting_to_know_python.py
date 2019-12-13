28c7afd7a275224e69fbb53a5cee74e352f2d3b1
olebogeng <seribeolebogeng@gmail.com>
2019-12-13 14:41:57 +0200

    test changes

--- test_getting_to_know_python.py
+++ test_getting_to_know_python.py
@@ -1,4 +1,4 @@
 from getting_to_know_python import list_all_js_function_names
 #testing to see if it returns function names
 def test_function():
-    assert list_all_js_function_names('script.js') == [' promptUser', 'Array.prototype.memory_card_shuffle ', ' newBoard', ' memoryFlipCard', ' flip2Back']
+    assert list_all_js_function_names('script.js') == [{'name': ' promptUser', 'starting line': 7, 'end_line': 13}, {'name': 'Array.prototype.memory_card_shuffle ', 'starting line': 15, 'end_line': 23}, {'name': ' newBoard', 'starting line': 25, 'end_line': 35}, {'name': ' flip2Back', 'starting line': 64, 'end_line': 80}]
