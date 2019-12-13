19151e6ab5dd57cef3e7d0bb7f1d61e6ecbc14d2
olebogeng <seribeolebogeng@gmail.com>
2019-12-13 14:34:52 +0200

    changes

--- getting_to_know_python.py
+++ getting_to_know_python.py
@@ -6,32 +6,39 @@ def list_all_js_function_names(path_to_js_file):
     """
     #creating a list that will have lines with function names
     myfunctions=[]
-
-
+    
     #reading into the file    
     with open('script.js') as f:
         read_datalines =f.readlines()
 
-    
+    dict_functions={}
+    counter=0
     for line in read_datalines:
+        counter+=1
         if 'function' in line:
-            myfunctions.append(line)
-            # print(line)
-    # print(myfunctions)        
+            names_list = []
+            name=line.split("()")[0].split("=")[0].split("(")[0].split("function")[-1]
+            dict_functions['name']=name
+        
+    
+            starting_line = counter
+            dict_functions['starting line']=starting_line
+            
+            
 
-    names_list = []
-    for each_item in myfunctions:
+        if line.startswith('}'):
+            end_line=counter
+            dict_functions['end_line']=end_line 
+            myfunctions.append(dict_functions) 
+            dict_functions={}
 
-        arr = (each_item.split("()")[0]
-        .split("=")[0]
-        .split("(")[0]
-        .split("function")[-1]
-        )
-        names_list.append(arr)
-    return names_list
+    
+    return myfunctions
 print(list_all_js_function_names('script.js'))   
   
-# if 'function' in line:
 
-# name_dict = {}
-# name_dict['name'] = arr
+
+
+
+    
+
